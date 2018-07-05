4d104f0a75ec18accfb926462e84d1b8f5e468ea
GuruKanzler <magnus.bergqvist@gmail.com>
2018-07-05 21:56:22 +0200

    new r files with latest changes

--- plot3.R
+++ plot3.R
@@ -12,51 +12,25 @@ library(ggplot2)
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-agg_type_year <- aggregate(NEI$Emissions, list(NEI$type, NEI$year),mean)
-g <- ggplot(agg_type_year, aes(y=x, x=Group.2))+geom_point(aes(colour = factor(Group.1)), size = 4)
-g+geom_line(aes(group=Group.1,colour=Group.1))
-
-
 # Subsetting
 BaltEM <- subset(NEI, fips == 24510)
 
 # Factoring $year
 BaltEM$year <- factor(BaltEM$year, levels = c("1999", "2002", "2005", "2008"))
 
-png(filename='~/Exploratory_Data_Analysis/Assignment_2/plot3.png', width=800, height=500, units='px')
-
-ggplot(data=MD, aes(x=year, y=log(Emissions))) + facet_grid(. ~ type) + guides(fill=F) +
-  geom_boxplot(aes(fill=type)) + stat_boxplot(geom ='errorbar') +
-  ylab(expression(paste('Log', ' of PM'[2.5], ' Emissions'))) + xlab('Year') + 
-  ggtitle('Emissions per Type in Baltimore City, Maryland') +
-  geom_jitter(alpha=0.10)
-
-dev.off()
-
-head(BaltEM)
-
-# Barplotting (optional)
-ggplot(data = BaltEM, aes(x = year, y = log(Emissions))) +
-  facet_grid(. ~type) +
-  guides(fill=F) + 
-  geom_boxplot(aes(fill = type)) +
-  stat_boxplot(geom = 'errorbar') +
-  ylab(expression(paste('Log', ' of PM'[2.5], ' Emissions'))) +
-  xlab('Year') +
-  ggtitle("Emissions per type; Baltimore") +
-  geom_jitter(alpha = 0.05)
+# Barplotting
+g <- ggplot(data = BaltEM, aes(x = year, y = log(Emissions))) +
+      facet_grid(. ~type) +
+      guides(fill = F) + 
+      geom_boxplot(aes(fill = type)) +
+      stat_boxplot(geom = 'errorbar') +
+      ylab(expression(paste('Log', ' of PM'[2.5], ' Emissions'))) +
+      xlab('Year') +
+      ggtitle("Emissions per type; Baltimore") +
+      geom_jitter(alpha = 0.05)
+print(g)
 
 # Save barplot to file
-png('plot3.png', width = 800, height = 500, units = "px")
-
-ggplot(data = BaltEM, aes(x = year, y = log(Emissions))) +
-  facet_grid(. ~type) +
-  guides(fill=F) + 
-  geom_boxplot(aes(fill = type)) +
-  stat_boxplot(geom = 'errorbar') +
-  ylab(expression(paste('Log', ' of PM'[2.5], ' Emissions'))) +
-  xlab('Year') +
-  ggtitle("Emissions per type; Baltimore") +
-  geom_jitter(alpha = 0.05)
-
+png('plot3b.png', width = 800, height = 500, units = "px")
+print(g)
 dev.off()
--- plot6.R
+++ plot6.R
@@ -17,8 +17,35 @@ scc_vehicles <- SCC[v,]$SCC
 nei_vehicles <- NEI[NEI$SCC %in% scc_vehicles,]
 
 # Focus on Baltimore and  Los Angeles data
-nei_vehicles_baltimore <- nei_vehicles[nei_vehicles$fips == 24510,]
-nei_vehicles_losangeles <- nei_vehicles[nei_vehicles$fips == 06037,]
+nei_vehicles_baltimore <- nei_vehicles[nei_vehicles$fips == "24510",]
+nei_vehicles_losangeles <- nei_vehicles[nei_vehicles$fips == "06037",]
+
+# Add city column
+nei_vehicles_baltimore$city <- "Baltimore"
+nei_vehicles_losangeles$city <- "LosAngeles"
+
+# Combine data
+nei_vehicles_bothcities <- rbind(nei_vehicles_baltimore, nei_vehicles_losangeles)
+
+# Create and plot graph
+g <- ggplot(nei_vehicles_bothcities, aes(x = factor(year), y = Emissions, fill = city)) +
+  geom_bar(aes(fill = year), stat = "identity") +
+  facet_grid(scales = "free", space = "free", .~city) +
+  guides(fill = F) + 
+  theme_bw() +
+  labs(x = "year", y = expression("Total PM"[2.5]*" Emission")) + 
+  labs(title = expression("PM"[2.5]*" Motor Vehicle Emissions in Baltimore & LA, 1999-2008"))
+
+print(g)
+
+# Save plot to file
+png('plot6.png', width = 800, height = 500, units = "px")
+print(g)
+dev.off()
+
+
+
+
 
 
 
